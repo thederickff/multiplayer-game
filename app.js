611c9a8cbc72d535bdd336ca77f7c44ab6c4c6bb
derickfelix <derickfelix4321@gmail.com>
2018-12-23 13:02:27 -0500

    add keyboard interactivity

--- app.js
+++ app.js
@@ -13,34 +13,82 @@ serv.listen(2000);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
 var fps = 1000 / 30;
 
-var io = require('socket.io')(serv, {});
+var Player = function(id) {
+    var self = {
+        id: id,
+        x: 250,
+        y: 250,
+        number: "" + Math.floor(10 * Math.random()),
+        pressingLeft: false,
+        pressingRight: false,
+        pressingUp: false,
+        pressingDown: false,
+        maxSpeed: 10
+    }
 
+    self.updatePosition = function() {
+        if (self.pressingUp) {
+            self.y -= self.maxSpeed;
+        }
+        if (self.pressingDown) {
+            self.y += self.maxSpeed;
+        }
+        if (self.pressingLeft) {
+            self.x -= self.maxSpeed;
+        }
+        if (self.pressingRight) {
+            self.x += self.maxSpeed;
+        }
+    }
+
+    return self;
+}
+
+var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function (socket) {
     socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket.number = "" + Math.floor(10 * Math.random());
+
+    var player = Player(socket.id);
 
     SOCKET_LIST[socket.id] = socket;
+    PLAYER_LIST[socket.id] = player;
+
+    socket.on('keyPress', function(data) {
+        switch (data.inputId) {
+            case 'up':
+                player.pressingUp = data.state;
+                break;
+            case 'down':
+                player.pressingDown = data.state;
+                break;
+            case 'left':
+                player.pressingLeft = data.state;
+                break;
+            case 'right':
+                player.pressingRight = data.state;
+                break;
+        }
+    });
 
     socket.on('disconnect', function() {
         delete SOCKET_LIST[socket.id];
+        delete PLAYER_LIST[socket.id];
     });
 });
 
 setInterval(function () {
     var pack = [];
 
-    for (var i in SOCKET_LIST) {
-        var socket = SOCKET_LIST[i];
-        socket.x++;
-        socket.y++;
+    for (var i in PLAYER_LIST) {
+        var player = PLAYER_LIST[i];
+        player.updatePosition();
         pack.push({
-            x: socket.x,
-            y: socket.y,
-            number: socket.number
+            x: player.x,
+            y: player.y,
+            number: player.number
         });
     }
 
