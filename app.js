3513dfa7b6163b3582716d0ccf7c7104fd3126d7
derickfelix <derickfelix4321@gmail.com>
2018-12-22 21:17:57 -0500

    add basic usage for socket.io

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ app.get('/', function (req, res) {
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(2000);
+
+console.log("Server started.");
+
+var io = require('socket.io')(serv, {});
+
+io.sockets.on('connection', function (socket) {
+    console.log("Socket connection.");
+    
+    socket.on('happy', function (data) {
+        var msg = "Happy because " + data.reason;
+        console.log(msg);
+        socket.emit('serverMsg', msg);
+    });
+
+});
